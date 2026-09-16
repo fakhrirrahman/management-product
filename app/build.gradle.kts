412beb44ee9fa7a0c9b427c758e07524eeecb985
fakhrirrahman <fakhrirrahman7@gmail.com>
2026-09-16 09:53:51 +0800

    fix: build graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,9 +23,9 @@ android {
 
     buildTypes {
         release {
-            optimization {
-                enable = false
-            }
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
